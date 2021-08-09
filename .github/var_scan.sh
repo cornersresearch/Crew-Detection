c366d3b1d41002747f1cce2c0917dfed960e1337
Adam Shelton <aqs@northwestern.edu>
2021-08-09 09:47:21 -0400

    Update var_scan with correct exit codes

--- .github/var_scan.sh
+++ .github/var_scan.sh
@@ -2,8 +2,9 @@ bad_vars="$(ag -Rin "^( |\t)*(\w{1,3}|data) *(=|<-).*$" .)"
 if ! test -z "$bad_vars"
 then
   echo "Unsavory variable names found"
-  touch ".github/linting_fail"
   echo "$bad_vars"
+  exit 1
 else
   echo "You chose quality variable names, nice!"
+  exit 0
 fi
